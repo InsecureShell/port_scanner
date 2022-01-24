b16fca14d4db30476be75c5b67451bb456918fea
Sasha Mortimer <6824835+Shaefa@users.noreply.github.com>
2022-01-23 19:00:02 -0500

    add socket connection code

--- port_scanner.py
+++ port_scanner.py
@@ -1,1 +1,17 @@
+import socket
+
+
+def port_scan(port, target, sock):
+    try:
+        connection = sock.connect((target, port))
+        return True
+    except:
+        return False
+
+
+def main():
+    pass
+
+
 if __name__ == '__main__':
+    main()
